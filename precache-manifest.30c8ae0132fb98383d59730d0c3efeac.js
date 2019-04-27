f429c80d81d22ee267bc4b7e0ccbe8b2f3a98b7b
Jenny Veens <jennyveens@gmail.com>
2019-04-27 08:43:38 -0400

    Updates

---
+++ precache-manifest.30c8ae0132fb98383d59730d0c3efeac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/color-combo/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "77ed61677ab519d74638",
+    "url": "/color-combo/static/js/runtime~main.15ce33ea.js"
+  },
+  {
+    "revision": "2b2565ddccbd2b68566a",
+    "url": "/color-combo/static/js/main.1ac49eec.chunk.js"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/color-combo/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "2b2565ddccbd2b68566a",
+    "url": "/color-combo/static/css/main.38786f3f.chunk.css"
+  },
+  {
+    "revision": "dbaf85c90945ea5c5f5da693e91b1204",
+    "url": "/color-combo/index.html"
+  }
+];
